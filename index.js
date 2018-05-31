7fd1e924be8cd862208fb1d8fa62a7e678a5fac9
Tobide <helitobide@gmail.com>
2018-06-01 00:53:07 +0200

    Ajout de get et add pour userFeedback

--- index.js
+++ index.js
@@ -8,26 +8,19 @@ var app = express();
 
 var PORT = process.env.PORT || 8080;
 
-/*
-var con = mysql.createConnection({
+var db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'assistant_mini'
+  database: 'assistant_utt'
 });
 
-con.connect(function(err){
+db.connect(function(err){
   if (err) throw err;
   console.log('Connexion à la base donnée OK')
 });
-*/
 
 app.use(bodyParser.json());
-
-
-function ajouterMessageFeedback(req, res, next){
-
-}
 
 //Juste une fonction qui permet d'afficher dans la console le contenu de la requête et de la renvoyer
 function echoRequete(req, res, next){
@@ -36,6 +29,29 @@ function echoRequete(req, res, next){
   console.log(reqTotal);
 
   next();
+}
+
+function addUserFeedback(nom, contenu){
+  var requete = 'INSERT INTO feedback_utilisateurs (nom_utilisateur, contenu) VALUES ("' + mysql.escape(nom) + '", "' + mysql.escape( contenu ) + '")';
+  db.query(requete, function(err,result, fields){
+    if (err) throw err;
+    console.log("Ajout de %s : %s", nom, contenu);
+  });
+}
+
+function getUserFeedback(option){
+  var requete = 'SELECT * FROM feedback_utilisateurs'
+  var resultats;
+  var test = "test";
+
+  db.query(requete, function(err, result, fields){
+    if(err) throw err;
+    resultats = result;
+  });
+
+  console.log(resultats);
+
+  return resultats;
 }
 
 function handleError(err, req, res, next){
@@ -47,7 +63,42 @@ function handleError(err, req, res, next){
   next();
 }
 
-app.post('/echo', echoRequete, function(req, res){ } );
+
+app.post('/echo', echoRequete, function(req, res){});
+
+app.post('/add/userFeedback', function(req, res){
+  if(req.body.nomUtilisateur != undefined & req.body.contenu != undefined){
+    addUserFeedback(req.body.nomUtilisateur, req.body.contenu);
+    res.status(201);
+    res.send(req.body);
+  }
+  else if(req.body.nomUtilisateur == undefined & req.body.contenu != undefined){
+    addUserFeedback("Anonyme", req.body.contenu);
+    res.status(201);
+    res.send(req.body);
+  }
+
+  // else{
+  //   res.statusCode = 400;
+  //   res.json({"error":"Bad body"});
+  // }
+
+});
+
+app.get('/', function(req, res){
+  res.send(undefined);
+})
+
+app.get('/get/userFeedback', function(req, res){
+  var requete = 'SELECT * FROM feedback_utilisateurs'
+
+  db.query(requete, function(err, result, fields){
+    if(err) throw err;
+    res.send(result);
+  });
+
+});
+
 app.use(handleError);
 
 app.listen(PORT, () => console.log("En attente sur le port " + PORT));
