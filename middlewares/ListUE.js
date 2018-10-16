89ef19999fbdfffe69d038bb5948dc1709d496b6
Tobide <helitobide@gmail.com>
2018-10-17 00:51:54 +0200

    Debugging /ue endpoint

--- middlewares/ListUE.js
+++ middlewares/ListUE.js
@@ -2,6 +2,8 @@
 
 module.exports.validateResult = function validateResult(req, res) {
   if (res.locals.queryResult || res.locals.queryResultArray.length > 0) {
+    console.log('Fetched :');
+    console.log(JSON.stringify(res.locals.queryResult, null, 2));
     res.json(res.locals.queryResult);
   } else {
     res.status(404).send({ error: 'No results' });
