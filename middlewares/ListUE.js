411103804ee497f3b0dc5b7e4c2aac7645cf9644
Tobide <helitobide@gmail.com>
2018-10-17 01:10:48 +0200

    Debugging /ue endpoint

--- middlewares/ListUE.js
+++ middlewares/ListUE.js
@@ -1,9 +1,17 @@
 // Middleware file for ListUE controller
 
 module.exports.validateResult = function validateResult(req, res) {
+
+  // A changer c'est dégueulasse lol
+  const isNotEmptyArray = !res.locals.queryResultArray
+    ? false
+    : res.locals.queryResultArray > 0
+    ? true
+    : false;
+
   if (res.locals.queryResult) {
     res.json(res.locals.queryResult);
-  } else if (res.locals.queryResultArray.length > 0) {
+  } else if (isNotEmptyArray) {
     res.json(res.locals.queryResultArray);
   } else {
     res.status(404).send({ error: 'No results' });
